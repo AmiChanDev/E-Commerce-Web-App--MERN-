5354f40a3bfc7ceb9b47286f2d87c51e186b0cad
AmiChanDev <gamageamantha@gmail.com>
2025-11-28 20:50:24 +0530

    feat: initialize backend with TypeScript, Express, and MongoDB setup
    - Updated package.json with project metadata, dependencies, and scripts for development and production.
    - Created db.ts for database configuration.
    - Implemented server.ts to set up Express server with middleware.
    - Added tsconfig.json for TypeScript configuration.

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import express, { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app: Application = express();
+
+
